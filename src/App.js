f504ac77fa5948fc865d4b391f28af41b392474e
Fe De <fede_piazza@hotmail.com>
2018-12-26 14:29:41 -0300

    componets added

--- src/App.js
+++ src/App.js
@@ -5,14 +5,12 @@ import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap
 import './App.css';
 import { TabContent, TabPane, Nav, NavItem, NavLink, Card, Button, CardTitle, CardText, Row, Col } from 'reactstrap';
 import classnames from 'classnames';
+import Sticker_1 from './sticker_1/app';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <div>
-          <Boton_desplegable/>
-        </div> 
         <div>
           <Example/>
         </div>
@@ -39,6 +37,7 @@ export default class Example extends React.Component {
       });
     }
   }
+
   render() {
     return (
       <div>
@@ -74,15 +73,13 @@ export default class Example extends React.Component {
         <TabContent activeTab={this.state.activeTab}>
 
           <TabPane tabId="1">
-            <Row>
-              <Col sm="12">
-                <h4>Tab 1 Contents</h4>
-              </Col>
-            </Row>
+              <div>
+                <Sticker_1/>
+              </div> 
           </TabPane>
 
           <TabPane tabId="2">
-          <Row>
+            <Row>
               <Col sm="12">
                 <h4>Tab 2 Contents</h4>
               </Col>
@@ -90,7 +87,7 @@ export default class Example extends React.Component {
           </TabPane>
 
           <TabPane tabId="3">
-          <Row>
+            <Row>
               <Col sm="12">
                 <h4>Tab 3 Contents</h4>
               </Col>
@@ -104,121 +101,8 @@ export default class Example extends React.Component {
 }
 
 
-
-class Boton_desplegable extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      dropdownOpen: false
-    };
-  }
-
-  toggle() {
-    this.setState(prevState => ({
-      dropdownOpen: !prevState.dropdownOpen
-    }));
-  }
-
-  render() {
-    return (
-      <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
-        <DropdownToggle caret>
-          Dropdown
-        </DropdownToggle>
-        <DropdownMenu>
-          <DropdownItem header>Opciones</DropdownItem>
-          <DropdownItem>Some Action</DropdownItem>
-          
-        </DropdownMenu>
-      </Dropdown>
-    );
-  }
-  /*
-          <DropdownItem disabled>Action (disabled)</DropdownItem>
-          <DropdownItem divider />
-          <DropdownItem>Foo Action</DropdownItem>
-          <DropdownItem>Bar Action</DropdownItem>
-          <DropdownItem>Quo Action</DropdownItem>
-  */
-}
-
 //--------------------------------------------------------------------------------------------------
 
-class Comida extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = { like: Boolean(this.props.like) };
-    this.handleLike = this.handleLike.bind(this);
-  };
 
-  handleLike(event) {
-    this.setState({ like: !this.state.like })
-    //console.log(event)
-    //console.log(this.state)
-  }
-  render() {
-    return (
-      <div className="comida">
-        <h1 className="bg-success">{this.props.nombre}</h1>
-        <p className="bg-info">
-         <i>{this.props.descripcion}</i>
-        </p>
-        <div>
-          <br />
-        </div>
-      </div>
-    )
-  }
-};
 
-class ListaComida extends React.Component {
-  state = {
-    comidas: [
-      'Tacos',
-      'Paella',
-      'Ceviche'
-    ],
-    data: []
-  };
-
-  constructor(props) {
-    super(props)
-    this.fetchAsync();
-  };
-
-  async fetchAsync() {
-    // await response of fetch call
-    let response = await fetch('http://localhost:8080/api/sticker/find?');
-    // only proceed once promise is resolved
-    //console.log(response);
-    let data = await response.json();
-
-    this.setState({ data: data.data })
-  }
-
-  render() {
-    return (<div className="centerBlock">
-      <header>
-        <h1>Ticket</h1>
-        <i>Total: {this.state.data.length}</i>
-      </header>
-      <div>
-        {
-          this.state.data.map(function (tike, i) {
-            console.log(tike, i);
-            return (
-              <Comida key={i}
-                nombre={tike.codigo}
-                descripcion={tike.descripcion}>
-              </Comida>
-            )
-          })
-        }
-      </div>
-    </div>)
-  }
-};
-
-export { ListaComida, App }
+export { App }
---
+++ src/sticker_1/app.js
@@ -0,0 +1,149 @@
+
+import React from 'react';
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import './app.css';
+
+class Sticker_1 extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      dropdownOpen: false
+    };
+  }
+
+  toggle() {
+    this.setState(prevState => ({
+      dropdownOpen: !prevState.dropdownOpen
+    }));
+  }
+
+  render() {
+    return (
+      <div>
+        <Boton_desplegable/>
+        <Lista_sticker/>
+      </div>
+
+    );
+  }
+}
+
+
+class Boton_desplegable extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      dropdownOpen: false
+    };
+  }
+
+  toggle() {
+    this.setState(prevState => ({
+      dropdownOpen: !prevState.dropdownOpen
+    }));
+  }
+
+  render() {
+    return (
+      <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+        <DropdownToggle caret>
+          Dropdown
+        </DropdownToggle>
+        <DropdownMenu>
+          <DropdownItem header>Opciones</DropdownItem>
+          <DropdownItem>Some Action</DropdownItem>
+          
+        </DropdownMenu>
+      </Dropdown>
+    );
+  }
+  /*
+          <DropdownItem disabled>Action (disabled)</DropdownItem>
+          <DropdownItem divider />
+          <DropdownItem>Foo Action</DropdownItem>
+          <DropdownItem>Bar Action</DropdownItem>
+          <DropdownItem>Quo Action</DropdownItem>
+  */
+}
+
+class Tarjeta extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { like: Boolean(this.props.like) };
+    this.handleLike = this.handleLike.bind(this);
+  };
+
+  handleLike(event) {
+    this.setState({ like: !this.state.like })
+    //console.log(event)
+    //console.log(this.state)
+  }
+  render() {
+    return (
+      <div className="comida">
+        <h1 className="bg-success">{this.props.nombre}</h1>
+        <p className="bg-info">
+         <i>{this.props.descripcion}</i>
+        </p>
+        <div>
+          <br />
+        </div>
+      </div>
+    )
+  }
+};
+
+class Lista_sticker extends React.Component {
+  state = {
+    comidas: [
+      'Tacos',
+      'Paella',
+      'Ceviche'
+    ],
+    data: []
+  };
+
+  constructor(props) {
+    super(props)
+    this.fetchAsync();
+  };
+
+  async fetchAsync() {
+    // await response of fetch call
+    let response = await fetch('http://localhost:8080/api/sticker/find?');
+    // only proceed once promise is resolved
+    //console.log(response);
+    let data = await response.json();
+
+    this.setState({ data: data.data })
+  }
+
+  render() {
+    return (<div className="centerBlock">
+      <header>
+        <h1>Sticker 1</h1>
+        <i>Total: {this.state.data.length}</i>
+      </header>
+      <div>
+        {
+          this.state.data.map(function (tike, i) {
+            console.log(tike, i);
+            return (
+              <Tarjeta key={i}
+                nombre={tike.codigo}
+                descripcion={tike.descripcion}>
+              </Tarjeta>
+            )
+          })
+        }
+      </div>
+    </div>)
+  }
+};
+
+
+export default Sticker_1;
